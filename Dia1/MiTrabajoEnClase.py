33089953f394ab6fc419415d5cddb956f5f0a4f4
DiegoGarcia <dgarciarodriguez370@gmail.com>
2024-04-09 12:59:45 -0500

    Dia1

---
+++ Dia1/MiTrabajoEnClase.py
@@ -0,0 +1,50 @@
+#------------------------------
+#-----Dia 1 CHEAT SHEET PYTHON-----
+#------------------------------
+
+#Imprimir Hola Mundo
+print("Hola Mundoooooooooooo")
+
+#Datos Primitivos
+
+#Numero
+numerito = 1 #nombreVariable = valor
+print(numerito) #imprimir(variable)
+print(type(numerito)) #imprimir(tipo(variable))
+
+#Decimal
+numeritoDecimal = 1.1
+print(numeritoDecimal)
+print(type(numeritoDecimal))
+
+#Booleano
+miBooleanito = True
+print(miBooleanito)
+print(type(miBooleanito))
+
+#Cadena de texto
+texto = "MI TIBU"
+print(texto)
+print(type(texto))
+
+#Ingresa por teclado la infomarcion(input)
+#ingreso
+numeroF = input("ingrese tu numero favorito ")
+
+numeroF =(int(numeroF))
+print("seguro que tu numero favorto es",numeroF,"si o no")
+
+print("si",numeroF,"es mi numero favorito")
+
+x = "mucho gusto"
+
+def myfunc():
+  print("Soy Diego " + x)
+
+myfunc()
+#Conversion de tipos de variable (tipos de variables primitivos)
+
+#Bucles For y While
+
+#Funciones (4 Tipos)
+#Desarrollado por Garcia Rodriguez - C.C 1.093.780.794
