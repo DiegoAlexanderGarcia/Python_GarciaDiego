d0ffcbade99040e4f0a3485e96f99667809fe5aa
DiegoGarcia <dgarciarodriguez370@gmail.com>
2024-04-12 12:54:06 -0500

    update

--- Dia4/ActividadClase.py
+++ Dia4/ActividadClase.py
@@ -1,10 +1,16 @@
+#defino el limite de monedas que tengo para pagar
 money = [1, 5, 10]
+#agrego un almacen para las monedas usadas
 selectedMoney = []
+#un contador para saber cuantas monedas use
 usedmoney=0
+#agrego la cantidad de cambio que decee
 Cash = int(input("Cantidad de cambio: "))
+#aqui pido que me traiga la moneda de mayor valor a pagar
 index = len(money) - 1
 
-while Cash > 0:
+#hago un bucle para que se cierre el programa cuando pague completo en 0
+while Cash > 0:    
     pay = Cash - money[index]
     if pay >= 0:
         Cash = pay
@@ -12,7 +18,7 @@ while Cash > 0:
         usedmoney += 1
     else:
         index -= 1
-
+#le doy salida a los datos
 print("Monedas devueltas:", selectedMoney)
 print("cantidad de monedas usadas: ",usedmoney)
   
